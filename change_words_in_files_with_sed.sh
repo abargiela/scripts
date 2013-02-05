d306aa8c9053db7dd316a1827545ccb9b3dc46a9
Alexandre <abargiela@sp.r7.com>
2013-02-05 18:18:07 -0200

    Bug detected backup files with names wrong

--- change_words_in_files_with_sed.sh
+++ change_words_in_files_with_sed.sh
@@ -14,6 +14,8 @@
 #       CREATED:  30/01/2013 15:08:52 PM BRT
 #      REVISION:  ---
 #      Bug:	./change_words_in_files_with_sed.sh -f /tmp/test.txt -o /home\/something\/somethingelse -n /home/www Doesn't word yet need to something like: ./change_words_in_files_with_sed.sh -f /tmp/test.txt -o "\/home\/something\/somethingelse" -n "\/home\/www" 
+#      Bug: If you execute N times the script, it cause a problema with the backup file:  /tmp/teste/info.txt-backup-05022013-1360095125-backup-05022013-1360095126-backup-05022013-1360095126-backup-05022013-1360095127-backup-05022013-1360095127-backup-05022013-1360095127-backup-05022013-1360095255 /tmp/teste/info.txt-backup-05022013-1360095125-backup-05022013-1360095126-backup-05022013-1360095126-backup-05022013-1360095127-backup-05022013-1360095127-backup-05022013-1360095127-backup-05022013-1360095255-backup-05022013-1360095256
+# It creates a lot of backups with strange names.
 #===============================================================================
 
 getHelp(){
