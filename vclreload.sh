931eec3d4cd8906d85e314ede27cb0e46cddde88
Alexandre <abargiela@sp.r7.com>
2013-02-15 14:02:21 -0200

    Adjust commands

--- vclreload.sh
+++ vclreload.sh
@@ -5,11 +5,11 @@
 # Modificated by A. Bargiela (alexandre@bargiela.com.br) in 15/02/2013
 # Adjusted to work getting the vcl(FILE)/varnishadm port(HOSTPORT) without intervention, just plug and play
 
-FILE=$(cat /etc/default/varnish|grep "\-f"|head -1| awk '{print $2}')
+FILE=$(grep "\-f" /etc/default/varnish |head -1| awk '{print $2}')
 
 # Hostname and management port
 # (defined in /etc/default/varnish or on startup)
-HOSTPORT=$(cat /etc/default/varnish|grep "\-T" | awk '{print $3}')
+HOSTPORT=$(grep "\-T" /etc/default/varnish | awk '{print $3}')
 NOW=`date +%s`
 
 error()
