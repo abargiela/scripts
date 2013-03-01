13795d299524b59d096c0ec300a3da2ae35c7fd0
Alexandre <abargiela@sp.r7.com>
2013-03-01 11:12:24 -0300

    BugFix: Adjust backup versioning

--- change_words_in_files_with_sed.sh
+++ change_words_in_files_with_sed.sh
@@ -14,8 +14,6 @@
 #       CREATED:  30/01/2013 15:08:52 PM BRT
 #      REVISION:  ---
 #      Bug:	./change_words_in_files_with_sed.sh -f /tmp/test.txt -o /home\/something\/somethingelse -n /home/www Doesn't word yet need to something like: ./change_words_in_files_with_sed.sh -f /tmp/test.txt -o "\/home\/something\/somethingelse" -n "\/home\/www" 
-#      Bug: If you execute N times the script, it cause a problema with the backup file:  /tmp/teste/info.txt-backup-05022013-1360095125-backup-05022013-1360095126-backup-05022013-1360095126-backup-05022013-1360095127-backup-05022013-1360095127-backup-05022013-1360095127-backup-05022013-1360095255 /tmp/teste/info.txt-backup-05022013-1360095125-backup-05022013-1360095126-backup-05022013-1360095126-backup-05022013-1360095127-backup-05022013-1360095127-backup-05022013-1360095127-backup-05022013-1360095255-backup-05022013-1360095256
-# It creates a lot of backups with strange names.
 #===============================================================================
 
 getHelp(){
@@ -67,13 +65,16 @@ done
 
 search_dir(){
     # Get data and parse
-    TMP_FILE="/tmp/tmp.file"
-    DATA=`egrep ${OLD} ${DIR} -R  | awk -F \: '{print $1}' | sed 's/ //g'`
-    echo $DATA | tr ' ' '\n' > ${TMP_FILE}
+    TMP_FILE="/tmp/tmp.file";
+    DATA=`egrep ${OLD} ${DIR} -R  | awk -F \: '{print $1}' | sed 's/ //g'`;
+    echo $DATA | tr ' ' '\n' > ${TMP_FILE};
+
+    for x in `cat ${TMP_FILE}`; do
+        cp -a  ${x[@]} ${x[@]}-backup-`date +%d%m%Y-%s`;
+    done
 
     # Get each file and execute the file backup and change the content
-    for i in `cat ${TMP_FILE}`;do
-        cp -a ${i[@]} ${i[@]}-backup-`date +%d%m%Y-%s`;
+    for i in `cat ${TMP_FILE}`;do        
         sed -i "s/"${OLD}"/"${NEW}"/g" ${i[@]};
     done
 }
