48e15dec62fa04f8578c9f7cf79dbcf2c5960c50
Alexandre Bargiela <alexandre@bargiela.com.br>
2016-05-06 10:17:09 -0300

    Added files via upload

---
+++ get_permissions_to_fix.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+function help {
+  echo -e "
+  Usage: bash `basename "$0"` FILE|DIR "
+}
+
+function get_permissions {
+for i in `find $1` ;do 
+	echo -e "chmod `stat -c '%a %n' $i` \nchown `stat -c '%U:%G' $i` $i" >> /tmp/permissions_$(date "+%Y%m%d").sh
+done
+	echo "Execute: bash /tmp/permissions_$(date "+%Y%m%d").sh"
+}
+
+case $1 in
+  $1)
+    get_permissions $1
+    ;;
+  help)
+    help
+    ;;
+  *)
+    echo "Type a valid option or help for help"
+    exit 1
+    ;;
+esac
