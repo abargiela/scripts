3844a6b710708d91bb26d4dbaf39b07bf881ad2d
Alexandre Bargiela <alexandre@bargiela.com.br>
2016-05-06 10:21:16 -0300

    Update get_permissions_to_fix.sh

--- get_permissions_to_fix.sh
+++ get_permissions_to_fix.sh
@@ -2,22 +2,23 @@
 
 function help {
   echo -e "
-  Usage: bash `basename "$0"` FILE|DIR "
+  Usage: bash `basename "$0"` FILE|DIR 
+"
 }
 
 function get_permissions {
 for i in `find $1` ;do 
-	echo -e "chmod `stat -c '%a %n' $i` \nchown `stat -c '%U:%G' $i` $i" >> /tmp/permissions_$(date "+%Y%m%d").sh
+        echo -e "chmod `stat -c '%a %n' $i` \nchown `stat -c '%U:%G' $i` $i" >> /tmp/permissions_$(date "+%Y%m%d").sh
 done
-	echo "Execute: bash /tmp/permissions_$(date "+%Y%m%d").sh"
+        echo "Execute: bash /tmp/permissions_$(date "+%Y%m%d").sh"
 }
 
 case $1 in
+    -h|--help)
+    help
+    ;;
   $1)
     get_permissions $1
-    ;;
-  help)
-    help
     ;;
   *)
     echo "Type a valid option or help for help"
