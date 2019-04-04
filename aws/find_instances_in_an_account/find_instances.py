12615e049f44d90be89836f0ad9eb450e42f4e4a
Alexandre Bargiela <alexandre@bargiela.com.br>
2019-04-04 12:45:07 -0300

    Update find_instances.py

--- aws/find_instances_in_an_account/find_instances.py
+++ aws/find_instances_in_an_account/find_instances.py
@@ -1,6 +1,7 @@
 import boto3
 import re
 import sys
+
 # Requirements: python3
 # pip install boto3 re
 
@@ -11,9 +12,9 @@ instances_len = len(sys.argv)
 x = 0
 
 # Add the credentials path must be the full path
-cred="/home/user/.aws/credentials"
+cred = "/home/alexandre.bargiela/.aws/credentials"
 # Region you want
-region='us-east-1'
+region = "us-east-1"
 
 
 if instances_len <= 1:
@@ -21,27 +22,24 @@ if instances_len <= 1:
 else:
     pass
 
-#import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 with open(cred) as file:
     data = file.read()
     # get only the credentials [my-credential]
-    data = re.findall(r"\[.*\]",data)
+    data = re.findall(r"\[.*\]", data)
 
-    
     for i in data:
         # Count the parameters passed
         while instances_len >= x:
             x += 1
         # Removes [] from the profile
-        profile = re.sub(r'\[|\]','',i)
+        profile = re.sub(r"\[|\]", "", i)
         # uses the profile in the session
-        session = boto3.Session(profile_name=profile,region_name=region)
-        client = session.client('ec2')
+        session = boto3.Session(profile_name=profile, region_name=region)
+        client = session.client("ec2")
         for instancesids in instances_ids:
             try:
-                response = client.describe_instances(
-                InstanceIds = [instancesids],
-                )
-                print(f"\nInstance: {instancesids} found: {profile}\n{response}\n" )
-            except: 
-                print(f"Instance: {instancesids} not found: {profile}" )
+                response = client.describe_instances(InstanceIds=[instancesids])
+                print(f"\nInstance: {instancesids} found: {profile}\n{response}\n")
+            except:
+                print(f"Instance: {instancesids} not found: {profile}")
