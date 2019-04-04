1a965369fe12639602e8ae62ac8b2df4ce951b45
Alexandre Bargiela <alexandre@bargiela.com.br>
2019-04-04 10:35:46 -0300

    [Fix] Pass instances using parameters

--- aws/find_instances_in_an_account/find_instances.py
+++ aws/find_instances_in_an_account/find_instances.py
@@ -1,13 +1,17 @@
 import boto3
 import re
-
+import sys
 # Requirements: python3
 # pip install boto3 re
 
-# Add the instances you want to search, accept instances in multiple accounts
-instances_ids = ['i-xxxxxxxxx', 'i-xxxxxxxxx','i-xxxxxxxxx','i-xxxxxxxxx']
+
+instances_ids = list(sys.argv[1:])
+instances_len = len(sys.argv)
+
+x = 0
+
 # Add the credentials path must be the full path
-cred="/home/user/.aws/credentials"
+cred="/home/alexandre.bargiela/.aws/credentials"
 # Region you want
 region='us-east-1'
 
@@ -15,13 +19,17 @@ with open(cred) as file:
     data = file.read()
     # get only the credentials [my-credential]
     data = re.findall(r"\[.*\]",data)
+
+    
     for i in data:
+        # Count the parameters passed
+        while instances_len >= x:
+            x += 1
         # Removes [] from the profile
         profile = re.sub(r'\[|\]','',i)
         # uses the profile in the session
         session = boto3.Session(profile_name=profile,region_name=region)
         client = session.client('ec2')
-
         for instancesids in instances_ids:
             try:
                 response = client.describe_instances(
