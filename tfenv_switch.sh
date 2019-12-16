1b99eb37389b421ca400e8e83f829c0ee836a14b
Alexandre Bargiela <alexandre@bargiela.com.br>
2019-12-16 16:43:05 -0300

    Update tfenv_switch.sh

--- tfenv_switch.sh
+++ tfenv_switch.sh
@@ -6,6 +6,7 @@
 # tfenv_switch 
 
 function tfenv_switch(){
+    # Search for the string required_version 
     TF_FILE_TEST=$(
         grep required_version ${PWD}/*.tf | \
         grep -v \# | \
@@ -14,10 +15,12 @@ function tfenv_switch(){
         tail -n1
     )
     
+    # In some cases you can set in the required version 0.12, but to install you need set 0.12.0
     if [[ "${TF_FILE_TEST}" == "0.12" ]];then  
         TF_FILE_TEST="0.12.0"
     fi
 
+    # Install the version of terraform and switch to use it.
     tfenv install ${TF_FILE_TEST} && \
     tfenv use ${TF_FILE_TEST}
 }
